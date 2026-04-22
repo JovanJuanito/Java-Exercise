a34723e252309e0143f876da53a191dbe07e4ab9
Friederichie <jovanadikjordan@gmail.com>
2026-04-22 15:26:06 +0800

    bugfix
    new name added

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -3,5 +3,6 @@ public class FirstClass4Team {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01357057\"\n姓名：\"莊全桔\"");
         System.out.println("\"學號：\"01357038\"\n姓名：\"林顥軒\"");
+        System.out.println("\"學號：\"01357058\"\n姓名：\"黃顏富\"");
     }
 }
