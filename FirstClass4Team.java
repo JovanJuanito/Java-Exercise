b5b2f694b910a49895cfc6055faa7ceefb6d0703
linzero1010 <ericlin1010@gmail.com>
2026-04-22 15:23:06 +0800

    Update FirstClass4Team.java

--- FirstClass4Team.java
+++ FirstClass4Team.java
@@ -2,5 +2,6 @@ public class FirstClass4Team {
     public static void main(String[] args) {
         System.out.println("這是用來練習GitHub協作的程式!");
         System.out.println("\"學號：\"01357057\"\n姓名：\"莊全桔\"");
+        System.out.println("\"學號：\"01357038\"\n姓名：\"林顥軒\"");
     }
 }
